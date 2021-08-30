ff546905d9a6419a0e71ffdbc80c660be72376fb
Agustinus Ardhito <agustinus.ardhito@alumni.ui.ac.id>
2021-08-30 22:39:38 +0700

    improve for crypto currencies

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # tradesman
-Trading calculator to make you calculate your profit &amp; risk well.
+Trading calculator to make you calculate your profit &amp; risk well. Well fit for counting PnL for :
+- Stocks
+- Cryptocurrencies
+- Reksadana
+- etc.
 
 ## How to Run
 - Clone the repository
 - Go inside it
-- Run : 
+- Run :
 ```
 $ chmod +x margin_calculator.rb
 $ ./margin_calculator.rb
+```
+- If you want to embed this to your terminal :
+```
+$ cp margin_calculator.rb tradesman
+$ sudo mv tradesman /usr/local/bin
+$ sudo ln -s /usr/local/bin/tradesman $PWD/tradesman
+$ tradesman
 ```
 
--- margin_calculator.rb
+++ margin_calculator.rb
@@ -3,19 +3,38 @@
 puts 'Welcome to margin calculator'
 puts '============================='
 
-puts 'Insert the initial modal amount:'
-modal = ARGF.gets.to_f
+puts 'Insert the stock price amount (1 stock): '
+stock_price = ARGF.gets.to_f
+puts 'Insert the stock shares that you have:'
+stock_amount = ARGF.gets.to_f
 puts 'Insert the stop profit value (%):'
 profit_percentage = ARGF.gets.to_f
 puts 'Insert the stop loss value (%):'
 loss_percentage = ARGF.gets.to_f
 
 # process the percentage
-profit_gross = modal + ((profit_percentage / 100) * modal)
-loss_gross = modal - ((loss_percentage / 100) * modal)
+assets = stock_price * stock_amount
 
-puts "Your stop profit amount : #{profit_gross}"
-puts "Your stop loss amount : #{loss_gross} "
+nett_profit = (profit_percentage / 100) * assets
+profit_gross = assets + nett_profit
+
+nett_loss = (loss_percentage / 100) * assets
+loss_gross = assets - nett_loss
+stock_price_after_profit = profit_gross / stock_amount
+stock_price_after_loss = loss_gross / stock_amount
+
+puts "\nASSETS"
+puts "Your total assets: #{assets}"
+
+puts "\nPOTENTIAL GAIN"
+puts "The profit gross amount: #{profit_gross}"
+puts "Nett potential profit: #{nett_profit}"
+puts "So, set your stop profit amount to #{stock_price_after_profit}"
+
+puts "\nPOTENTIAL LOSS"
+puts "The loss gross amount: #{loss_gross}"
+puts "Nett potential loss: #{nett_loss}"
+puts "So, set your stop loss amount to #{stock_price_after_loss}"
 
 puts '============================='
 puts 'Thank you. Please trade carefully :D'
